ed1dcece90425d122670146f5d8c8835e7ebb497
Lee Dawon <dawon337@gmail.com>
2022-12-05 14:40:38 +0900

    TIL2023 repo to do_it_django repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# do_it_django
-do it django
---
+++ venvs/mysite.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd C:\Users\USER\Desktop\leedawon\TIL_github\do_it_django\projects\mysite
+C:\Users\USER\Desktop\leedawon\TIL_github\do_it_django\venvs\mysite\Scripts\activate
+
+
+
