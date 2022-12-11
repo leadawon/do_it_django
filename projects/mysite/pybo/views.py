264fb4cb672f35f122e9f64a14f31ee5f3f46bc9
Lee Dawon <dawon337@gmail.com>
2022-12-11 18:46:23 +0900

    django pybo : view : answer_create added

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -3,7 +3,32 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render,get_object_or_404 , redirect
+
+from django.utils import timezone
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    '''
+    pybo 목록 출력
+    '''
+    question_list = Question.objects.order_by("-create_date")
+    context = {"question_list": question_list}
+    return render(request, "pybo/question_list.html",context)
 
+def detail(request,question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question,pk=question_id) #여기서 id가 존재하지 않으면 에러
+    context = {"question":question}
+    return render(request,"pybo/question_detail.html",context)
+
+def answer_create(request,question_id):
+    '''
+    pybo 답변 등록
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get("content"),
+                               create_date = timezone.now())
+    return redirect("pybo:detail",question_id=question.id)
