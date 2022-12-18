cb9ba704b56d5f243059eaa51426d7521f01cb33
Lee Dawon <dawon337@gmail.com>
2022-12-18 19:51:45 +0900

    django pybo : template : question_form.html remove {{form.as_p}} adding question with no content -> alert error with save question subject
    problem : previous : question_list.html. can't fix

--- projects/mysite/pybo/forms.py
+++ projects/mysite/pybo/forms.py
@@ -5,3 +5,7 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['subject','content']
+        labels = {
+            'subject' : '제목',
+            'content' : '내용',
+        }
