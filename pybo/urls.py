b1d82a254ee2c3a7f9c7dc733ce864e8e9f70a47
Lee Dawon <dawon337@gmail.com>
2023-02-13 18:11:16 +0900

    question detail

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index ),
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -8,3 +8,9 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list' : question_list}
     return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk = question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/question_detail.html', context)
